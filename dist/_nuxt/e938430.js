3ae49021e6b9f4b5b3380b52ffb0ef8c343a01f1
Alex Deeley <aedeeley@gmail.com>
2023-07-10 00:37:38 -0600

    + font fixes + static back + dist

--- dist/_nuxt/e938430.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{268:function(n,t,e){"use strict";e.r(t);var l=e(42),component=Object(l.a)({},(function(){return(0,this._self._c)("main")}),[],!1,null,null,null);t.default=component.exports}}]);
