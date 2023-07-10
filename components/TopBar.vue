4f1bd73a2c9d8ebc56f777954216d120016b36e0
Alex Deeley <aedeeley@gmail.com>
2023-07-10 00:22:58 -0600

    + favicon

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="gradient-spectrum pt-1 pb-0 tracking-wider container fixed top-0 z-50"
+    class="gradient-spectrum pt-1 pb-0 tracking-wider container fixed top-0 z-50 w-screen"
   >
   </div>
 </template>
